42101bf2e6555fbb44e5666151afcb0da9990520
xinemata <contact@xin-xin.info>
2019-09-12 17:57:49 -0400

    Inital Commit
    Added Rotating Cube, Pressed2Rotate Cube, Light Toggle

---
+++ CoreLabObjectDemos/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+
+    public Light myLight;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // using get key up / down to keep the light on / off
+        if (Input.GetKeyDown ("space")) {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
+
+
+//NOTES
+//
+//        // "up", "down"
+//        
+//        if (Input.GetKey ("up")) {
+//                myLight.enabled = true;
+//            } 
+//            
+//        if (Input.GetKey ("down")){
+//                myLight.enabled = false;
+//        }
+
---
+++ CoreLabObjectDemos/Assets/Scripts/PressedRotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressedRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if(Input.GetKey("up")){
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+        }
+        
+        if(Input.GetKey("down")){
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime * -1);
+        }
+        
+    }
+}
