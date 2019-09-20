2238f021e7cd73688069eba7254eabd42346328a
Xin Xin <9159424+xinemata@users.noreply.github.com>
2019-09-19 20:08:45 -0400

    Added pick up and destroy demo

---
+++ CoreLabObjectDemos/Assets/Scripts/pickUpAndDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpAndDestroy : MonoBehaviour
+{
+    private int score = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp")
+        {
+            score = score + 1;
+            print("you have collected " + score + " points");
+            Destroy(other.gameObject);
+        }
+    }
+}
