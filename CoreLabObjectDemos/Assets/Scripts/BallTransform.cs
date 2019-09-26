83ee4005bde7a91e852451b45cef76b1eee1f4fc
xinemata <contact@xin-xin.info>
2019-09-26 16:09:41 -0400

    Added Jump Demo

---
+++ CoreLabObjectDemos/Assets/Scripts/BallTransform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTransform : MonoBehaviour
+{
+
+    public GameObject ball;
+    public float x = 3;
+    public float grow = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.transform.localScale = new Vector3(x,x,x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ball.transform.localScale += new Vector3(grow,grow,grow);
+
+        Debug.Log(ball.transform.localScale.x);
+        
+    }
+}
---
+++ CoreLabObjectDemos/Assets/Scripts/Jump.cs
@@ -0,0 +1,27 @@
+﻿    using UnityEngine;
+    
+    [RequireComponent(typeof(Rigidbody))]
+    public class Jump : MonoBehaviour {
+        public float jumpSpeed = 5f;
+        public bool isGrounded;
+        Rigidbody rb;
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+        void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.tag == ("Ground") && isGrounded == false)
+            {
+                isGrounded = true;
+            }
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0) && isGrounded)
+            {
+                rb.AddForce(new Vector3(0, 2, 0) * jumpSpeed, ForceMode.Impulse);
+                isGrounded = false;
+            }
+        }
+    }
