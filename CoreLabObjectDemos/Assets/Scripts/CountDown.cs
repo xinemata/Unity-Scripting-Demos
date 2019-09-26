368652c4e1a96cc91682e926fc87c3aaa5fa7295
xinemata <contact@xin-xin.info>
2019-09-26 17:28:22 -0400

    added text mesh countdown timer

--- CoreLabObjectDemos/Assets/Scripts/CountDown.cs
+++ CoreLabObjectDemos/Assets/Scripts/CountDown.cs
@@ -1,23 +1,38 @@
 ﻿    using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
-    using UnityEngine.UI;
-    using System.Threading;
-    public class CountDown : MonoBehaviour {
-      public int timeLeft = 60; //Seconds Overall
-      public Text countdown; //UI Text Object
-      void Start () {
+    using TMPro;
+
+public class CountDown : MonoBehaviour
+{
+    public TextMeshPro timer; 
+    public int timeLeft = 60; //Seconds Overall
+    void Start()
+    {
         StartCoroutine("LoseTime");
         Time.timeScale = 1; //Just making sure that the timeScale is right
-      }
+    }
+
       void Update () {
-        countdown.text = ("" + timeLeft); //Showing the Score on the Canvas
+
+        if(timeLeft >= 0){
+          // timeLeft--;
+          GetComponent<TextMeshPro>().text = ("" + timeLeft);
+        } else {
+
+        GetComponent<TextMeshPro>().text = ("You lost");
+        }
+
       }
-      //Simple Coroutine
+
+          //Simple Coroutine
       IEnumerator LoseTime()
       {
-        while (true) {
+        while (timeLeft>0) {
           yield return new WaitForSeconds (1);
           timeLeft--; 
         }
-      }}
+      }
+}
+
+
