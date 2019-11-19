8c0bd4a26411428dc5117b7b5ce99ab63f3c0bb4
xinemata <contact@xin-xin.info>
2019-11-19 12:59:36 -0500

    added raycast destroy and bump to new position

--- CoreLabObjectDemos/Assets/Scripts/raycast.cs
+++ CoreLabObjectDemos/Assets/Scripts/raycast.cs
@@ -18,9 +18,14 @@ public class raycast : MonoBehaviour
 
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 15f, Color.red);
 
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out bump, Mathf.Infinity)){
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out bump, Mathf.Infinity) && Input.GetKey("down")){
 
             Debug.Log("bump!");
+
+            Destroy(bump.collider.gameObject);
+
+            // transitions the bumped game obj to a new position
+            //bump.collider.gameObject.transform.position = new Vector3(0f, 0f ,0f);
 
         }
         
