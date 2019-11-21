00006002263244980c0e8fa25a907de3f0fff347
xinemata <contact@xin-xin.info>
2019-11-21 13:19:59 -0500

    added mouseDrag script

---
+++ CoreLabObjectDemos/Assets/Scripts/mouseDrag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseDrag : MonoBehaviour {
+
+    float distance = 10;
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        transform.position = objectPos;
+    }
+}
